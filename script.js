40e3009a854cab766d267b0c6d5fddf36cd336cc
GudLub <gudilinalu@yandex.ru>
2022-12-21 21:23:17 +0300

    first

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+const letter = document.querySelector('.letter');
+const squard = Array.from(document.querySelectorAll('.squard'));
 
+
+const query = [];
+const queryFinal = [];
+
+// добавляем цвет
+letter.addEventListener("click", (e) => { 
+  e.target.classList.add('squard__green');
+  query.push(e.target.id);
+  console.log(query);
+  return query;
+});
+
+// убираем из массива лишние клики (повторные)
+const queryFinal = query.filter(function (item, position, arr) {
+    return arr.lastIndexOf(item) === position; 
+  });
+
+  // убираем цвет
+squard.forEach( function (item) {
+    if (item.classList.contains('.squard__green')) {
+for (i = 0; i < 7; i++) {
+    squard.queryFinal[i].classList.remove('.squard__green');
+}
+    }
+})
